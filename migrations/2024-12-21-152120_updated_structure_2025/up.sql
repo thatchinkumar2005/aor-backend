7d2a1202680e60cb8b6857efa8e731ba97fece9d
thatchinkumar2005 <thatchinkumar2005@icloud.com>
2024-12-22 10:05:25 +0530

    Migrations and schema for new structure

---
+++ migrations/2024-12-21-152120_updated_structure_2025/up.sql
@@ -0,0 +1,51 @@
+-- New prop table
+CREATE TABLE prop (
+    id SERIAL PRIMARY KEY NOT NULL,
+    "range" INTEGER NOT NULL DEFAULT 0,
+    frequency INTEGER NOT NULL DEFAULT 0
+);
+
+-- Drop defender_type, mine_type, building_type columns and add category_id to block_type
+ALTER TABLE block_type
+    DROP COLUMN defender_type,
+    DROP COLUMN mine_type,
+    DROP COLUMN building_type,
+    ADD COLUMN category_id INTEGER NOT NULL;
+
+-- Deleting "name", range and freqency columns from defender_type, mine_type, and attacker_type
+ALTER TABLE defender_type
+    DROP COLUMN "name",
+    DROP COLUMN radius;
+
+ALTER TABLE mine_type
+    DROP COLUMN "name",
+    DROP COLUMN radius;
+
+ALTER TABLE attacker_type
+    DROP COLUMN "name";
+
+-- Adding prop_id column to defender_type, mine_type, building_type, and attacker_type
+ALTER TABLE building_type
+    ADD COLUMN prop_id INTEGER;
+
+ALTER TABLE defender_type
+    ADD COLUMN prop_id INTEGER;
+
+ALTER TABLE mine_type
+    ADD COLUMN prop_id INTEGER;
+
+ALTER TABLE attacker_type
+    ADD COLUMN prop_id INTEGER;
+
+--Add foreign key constraints for prop_id
+ALTER TABLE building_type
+    ADD CONSTRAINT fk_building_prop FOREIGN KEY (prop_id) REFERENCES prop(id);
+
+ALTER TABLE defender_type
+    ADD CONSTRAINT fk_defender_prop FOREIGN KEY (prop_id) REFERENCES prop(id);
+
+ALTER TABLE mine_type
+    ADD CONSTRAINT fk_mine_prop FOREIGN KEY (prop_id) REFERENCES prop(id);
+
+ALTER TABLE attacker_type
+    ADD CONSTRAINT fk_attacker_prop FOREIGN KEY (prop_id) REFERENCES prop(id);
---
+++ schema.rs
@@ -0,0 +1,254 @@
+// @generated automatically by Diesel CLI.
+
+pub mod sql_types {
+    #[derive(diesel::sql_types::SqlType)]
+    #[diesel(postgres_type(name = "block_category"))]
+    pub struct BlockCategory;
+
+    #[derive(diesel::sql_types::SqlType)]
+    #[diesel(postgres_type(name = "item_category"))]
+    pub struct ItemCategory;
+}
+
+diesel::table! {
+    artifact (map_space_id) {
+        map_space_id -> Int4,
+        count -> Int4,
+    }
+}
+
+diesel::table! {
+    attack_type (id) {
+        id -> Int4,
+        #[max_length = 255]
+        att_type -> Varchar,
+        attack_radius -> Int4,
+        attack_damage -> Int4,
+    }
+}
+
+diesel::table! {
+    attacker_type (id) {
+        id -> Int4,
+        max_health -> Int4,
+        speed -> Int4,
+        amt_of_emps -> Int4,
+        level -> Int4,
+        cost -> Int4,
+        prop_id -> Nullable<Int4>,
+    }
+}
+
+diesel::table! {
+    use diesel::sql_types::*;
+    use super::sql_types::ItemCategory;
+
+    available_blocks (id) {
+        block_type_id -> Nullable<Int4>,
+        user_id -> Int4,
+        attacker_type_id -> Nullable<Int4>,
+        emp_type_id -> Nullable<Int4>,
+        category -> ItemCategory,
+        id -> Int4,
+    }
+}
+
+diesel::table! {
+    use diesel::sql_types::*;
+    use super::sql_types::BlockCategory;
+
+    block_type (id) {
+        id -> Int4,
+        category -> BlockCategory,
+        category_id -> Int4,
+    }
+}
+
+diesel::table! {
+    building_type (id) {
+        id -> Int4,
+        #[max_length = 255]
+        name -> Varchar,
+        width -> Int4,
+        height -> Int4,
+        capacity -> Int4,
+        level -> Int4,
+        cost -> Int4,
+        hp -> Int4,
+        prop_id -> Nullable<Int4>,
+    }
+}
+
+diesel::table! {
+    defender_type (id) {
+        id -> Int4,
+        speed -> Int4,
+        damage -> Int4,
+        level -> Int4,
+        cost -> Int4,
+        prop_id -> Nullable<Int4>,
+    }
+}
+
+diesel::table! {
+    emp_type (id) {
+        id -> Int4,
+        #[max_length = 255]
+        att_type -> Varchar,
+        attack_radius -> Int4,
+        attack_damage -> Int4,
+        cost -> Int4,
+        #[max_length = 255]
+        name -> Varchar,
+        level -> Int4,
+    }
+}
+
+diesel::table! {
+    game (id) {
+        id -> Int4,
+        attack_id -> Int4,
+        defend_id -> Int4,
+        map_layout_id -> Int4,
+        attack_score -> Int4,
+        defend_score -> Int4,
+        emps_used -> Int4,
+        damage_done -> Int4,
+        is_game_over -> Bool,
+        artifacts_collected -> Int4,
+        date -> Date,
+    }
+}
+
+diesel::table! {
+    level_constraints (level_id, block_id) {
+        level_id -> Int4,
+        no_of_blocks -> Int4,
+        block_id -> Int4,
+    }
+}
+
+diesel::table! {
+    levels_fixture (id) {
+        id -> Int4,
+        start_date -> Timestamp,
+        end_date -> Timestamp,
+        no_of_bombs -> Int4,
+        rating_factor -> Float4,
+        no_of_attackers -> Int4,
+    }
+}
+
+diesel::table! {
+    map_layout (id) {
+        id -> Int4,
+        player -> Int4,
+        level_id -> Int4,
+        is_valid -> Bool,
+    }
+}
+
+diesel::table! {
+    map_spaces (id) {
+        id -> Int4,
+        map_id -> Int4,
+        x_coordinate -> Int4,
+        y_coordinate -> Int4,
+        block_type_id -> Int4,
+    }
+}
+
+diesel::table! {
+    mine_type (id) {
+        id -> Int4,
+        damage -> Int4,
+        level -> Int4,
+        cost -> Int4,
+        prop_id -> Nullable<Int4>,
+    }
+}
+
+diesel::table! {
+    prop (id) {
+        id -> Int4,
+        range -> Int4,
+        frequency -> Int4,
+    }
+}
+
+diesel::table! {
+    shortest_path (base_id, source_x, source_y, dest_x, dest_y) {
+        base_id -> Int4,
+        source_x -> Int4,
+        source_y -> Int4,
+        dest_x -> Int4,
+        dest_y -> Int4,
+        next_hop_x -> Int4,
+        next_hop_y -> Int4,
+    }
+}
+
+diesel::table! {
+    simulation_log (game_id) {
+        game_id -> Int4,
+        log_text -> Text,
+    }
+}
+
+diesel::table! {
+    user (id) {
+        id -> Int4,
+        #[max_length = 255]
+        name -> Varchar,
+        #[max_length = 255]
+        email -> Varchar,
+        #[max_length = 255]
+        username -> Varchar,
+        is_pragyan -> Bool,
+        attacks_won -> Int4,
+        defenses_won -> Int4,
+        trophies -> Int4,
+        avatar_id -> Int4,
+        artifacts -> Int4,
+    }
+}
+
+diesel::joinable!(artifact -> map_spaces (map_space_id));
+diesel::joinable!(attacker_type -> prop (prop_id));
+diesel::joinable!(available_blocks -> attacker_type (attacker_type_id));
+diesel::joinable!(available_blocks -> block_type (block_type_id));
+diesel::joinable!(available_blocks -> emp_type (emp_type_id));
+diesel::joinable!(available_blocks -> user (user_id));
+diesel::joinable!(building_type -> prop (prop_id));
+diesel::joinable!(defender_type -> prop (prop_id));
+diesel::joinable!(game -> map_layout (map_layout_id));
+diesel::joinable!(level_constraints -> block_type (block_id));
+diesel::joinable!(level_constraints -> levels_fixture (level_id));
+diesel::joinable!(map_layout -> levels_fixture (level_id));
+diesel::joinable!(map_layout -> user (player));
+diesel::joinable!(map_spaces -> block_type (block_type_id));
+diesel::joinable!(map_spaces -> map_layout (map_id));
+diesel::joinable!(mine_type -> prop (prop_id));
+diesel::joinable!(shortest_path -> map_layout (base_id));
+diesel::joinable!(simulation_log -> game (game_id));
+
+diesel::allow_tables_to_appear_in_same_query!(
+    artifact,
+    attack_type,
+    attacker_type,
+    available_blocks,
+    block_type,
+    building_type,
+    defender_type,
+    emp_type,
+    game,
+    level_constraints,
+    levels_fixture,
+    map_layout,
+    map_spaces,
+    mine_type,
+    prop,
+    shortest_path,
+    simulation_log,
+    user,
+);
