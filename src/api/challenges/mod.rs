61e4e6728c1b01507def979166949e1061539ef3
thatchinkumar2005 <thatchinkumar2005@icloud.com>
2025-02-04 16:49:43 +0530

    feat: new get_challenge_maps route

--- src/api/challenges/mod.rs
+++ src/api/challenges/mod.rs
@@ -1,1 +1,17 @@
+use actix_web::{
+    web::{self, Data, Json, Path},
+    Responder, Result,
+};
+
+use super::PgPool;
+
 pub mod util;
+
+pub fn routes(cfg: &mut web::ServiceConfig) {
+    cfg.service(web::resource("/{id}").route(web::get().to(get_challenge_maps)))
+        .app_data(Data::new(web::JsonConfig::default().limit(1024 * 1024)));
+}
+
+async fn get_challenge_maps(challenge_id: Path<i32>, pool: Data<PgPool>) -> Result<impl Responder> {
+    Ok(Json(()))
+}
