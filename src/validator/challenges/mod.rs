f55683de69bbe996b47a77c77793345b8e801808
thatchinkumar2005 <thatchinkumar2005@icloud.com>
2025-02-06 19:32:45 +0530

    wip: fallguys challenge, logic for range and frequency increase

--- src/validator/challenges/mod.rs
+++ src/validator/challenges/mod.rs
@@ -14,18 +14,39 @@ pub fn attacker_movement_challenge_handle(
         if let Some(challenge_type) = challenge.challenge_type {
             match challenge_type {
                 ChallengeType::Maze => {
-                    for building in &game_state.buildings {
-                        if building.name == "Coin" {
-                            challenge.score += 1;
+                    if let Some(maze) = challenge.maze.as_mut() {
+                        for building in &game_state.buildings {
+                            if building.name == "Coin" {
+                                challenge.score += 1;
+                            }
+                        }
+                        if attacker_current.attacker_pos.x == maze.end_tile.x
+                            && attacker_current.attacker_pos.y == maze.end_tile.y
+                        {
+                            challenge.challenge_completed = true;
+                            game_state.in_validation = InValidation {
+                                message: "Maze Challenge Completed".to_string(),
+                                is_invalidated: true,
+                            }
                         }
                     }
-                    if attacker_current.attacker_pos.x == challenge.maze.end_tile.x
-                        && attacker_current.attacker_pos.y == challenge.maze.end_tile.y
-                    {
-                        challenge.challenge_completed = true;
-                        game_state.in_validation = InValidation {
-                            message: "Game Over".to_string(),
-                            is_invalidated: true,
+                }
+                ChallengeType::FallGuys => {
+                    if let Some(fall_guys) = challenge.fall_guys.as_mut() {
+                        if game_state.frame_no
+                            > fall_guys.last_intensity_update_tick
+                                + fall_guys.update_intensity_interval
+                        {
+                            for building in game_state.buildings.iter_mut() {
+                                if building.name == "Defender_Hut" {
+                                    building.range += fall_guys.hut_range_increment;
+                                    building.frequency += fall_guys.hut_frequency_increment;
+                                } else if building.name == "Sentry" {
+                                    building.range += fall_guys.sentry_range_increment;
+                                    building.frequency += fall_guys.sentry_frequency_increment;
+                                }
+                            }
+                            fall_guys.last_intensity_update_tick = game_state.frame_no;
                         }
                     }
                 }
